6ca1c7870056f119fc7e048535b0f5d06d31012f
amirhome <amir.email@gmail.com>
2021-12-06 13:44:04 +0300

    Read CSV file

---
+++ app/Classes/CSVReader.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Classes;
+
+use Exception;
+
+class CSVReader
+{
+
+    public function read(String $fileName){
+        try {
+            if (! file_exists(public_path($fileName))) {
+                throw new Exception("$fileName was not found");
+            }
+            // Read a CSV file
+            $handle = fopen(public_path($fileName), "r");
+
+            // Init result
+            $data = [];
+
+            // Optionally, you can keep the number of the line where
+            // the loop its currently iterating over
+            $lineNumber = 1;
+
+            // Iterate over every line of the file
+            while (($raw_string = fgets($handle)) !== false) {
+                // Parse the raw csv string: "1, a, b, c"
+                $row = str_getcsv($raw_string);
+
+                // into an array: ['1', 'a', 'b', 'c']
+                // And do what you need to do with every line
+                $data = $row;
+
+                // Increase the current line
+                $lineNumber++;
+            }
+            fclose($handle);
+            return $data;
+        }catch(Exception $e) {
+            return ['error'=>1, 'msg'=>$e->getMessage()];
+        }
+
+    }
+}
---
+++ app/Console/Commands/Script.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Classes\CSVReader;
+use Illuminate\Console\Command;
+
+class Script extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'calculate {file_name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get Input CSV file to calculate commission fee';
+
+    private CSVReader $csvReader;
+
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(CSVReader $csvReader)
+    {
+        $this->csvReader = $csvReader;
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $fileName = $this->argument('file_name');
+
+        $data = $this->csvReader->read($fileName);
+
+        if (isset($data['error'])) {
+            echo $data['msg'];
+            return Command::FAILURE;
+        } else {
+            return Command::SUCCESS;
+        }
+
+    }
+}
